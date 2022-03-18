89fa965ec1b8f2bfb42b5d3734dc6c124fc18231
QasHussain <Qas_24@hotmail.com>
2022-03-18 11:00:03 +0000

    Updated links

--- src/HomePage.js
+++ src/HomePage.js
@@ -35,7 +35,7 @@ function Home() {
     window.open("https://github.com/QasHussain/Final-Fantasy-Codex-V2");
   };
   const handleDesignClick = () => {
-    window.open("http://localhost:3000/designProcess");
+    window.open("https://my-website-5f1d8.web.app/designProcess");
   };
 
   return (
